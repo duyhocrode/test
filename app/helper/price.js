e01344ce191fc1caa089b78e2c828e15564bfe2e
duyhoccode <duyhoccode@gmail.com>
2022-12-21 00:17:28 +0700

    funny

--- app/helper/price.js
+++ app/helper/price.js
@@ -1,5 +1,6 @@
-const technicalindicators = require('technicalindicators');
+const technicalindicators = require("technicalindicators");
 const axios = require("axios");
+const talib = require("talib-binding");
 
 // Define the function that calculates the RSI value
 async function calculateRsi(symbol, interval, limit, rsiPeriod) {
@@ -30,5 +31,109 @@ async function calculateRsi(symbol, interval, limit, rsiPeriod) {
         console.error(error);
     }
 }
+
+
+async function calculateDivergenceIndicator(symbol, interval, limit, rsiPeriod, pivotLookbackRight, pivotLookbackLeft, minLookbackRange, maxLookbackRange) {
+    // Construct the API endpoint URL
+    const url = `https://fapi.binance.com/fapi/v1/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
+
+    try {
+        // Make a GET request to the API endpoint using axios
+        const response = await axios.get(url);
+
+        // Parse the response body to extract the closing prices and high/low values
+        const data = response.data;
+        const closingPrices = data.map(datapoint => datapoint[4]);
+        const highPrices = data.map(datapoint => datapoint[2]);
+        const lowPrices = data.map(datapoint => datapoint[3]);
+
+        // Calculate the RSI values using technicalindicators
+        const rsi = new technicalindicators.RSI({
+            values: closingPrices,
+            period: rsiPeriod
+        });
+        const rsiValues = rsi.getResult();
+
+        const pivotInputs = {
+            high: highPrices,
+            low: lowPrices,
+            close: closingPrices,
+        };
+        const pivotResults = await talib.execute({
+            name: "PIVOTPOINT",
+            startIdx: pivotLookbackLeft,
+            endIdx: pivotLookbackRight,
+            inReal: pivotInputs,
+        });
+        const pivotValues = pivotResults.result.outReal;
+
+        // Initialize variables to track divergence
+        let divergenceExists = false;
+        let divergenceType = '';
+
+        // Loop through the RSI and pivot values to check for divergence
+        for (let i = 0; i < rsiValues.length; i++) {
+            // Check that the lookback range is within the specified limits
+            const lookbackRange = i + 1;
+            if (lookbackRange < minLookbackRange || lookbackRange > maxLookbackRange) {
+                continue;
+            }
+
+            const currentRsi = rsiValues[i];
+            const previousRsi = rsiValues[i - 1];
+            const currentPivot = pivotValues[i];
+            const previousPivot = pivotValues[i - 1];
+            if (currentRsi < previousRsi) {
+                // Bullish divergence
+                if (currentPivot > previousPivot) {
+                    divergenceExists = true;
+                    divergenceType = 'bullish';
+                    break;
+                }
+            } else if (currentRsi > previousRsi) {
+                // Bearish divergence
+                if (currentPivot < previousPivot) {
+                    divergenceExists = true;
+                    divergenceType = 'bearish';
+                    break;
+                }
+            }
+        }
+
+        // Return the results
+        return {
+            exists: divergenceExists,
+            type: divergenceType
+        };
+    }  catch (error) {
+        // Handle the error
+        console.error(error);
+    }
+}
+
+// Define the function arguments
+const symbol = "BTCUSDT";
+const interval = "1h";
+const limit = 100;
+const rsiPeriod = 14;
+const pivotLookbackRight = 5;
+const pivotLookbackLeft = 5;
+const minLookbackRange = 60;
+const maxLookbackRange = 5;
+
+// Run the function every 1 second
+setInterval(async () => {
+    const result = await calculateDivergenceIndicator(
+        symbol,
+        interval,
+        limit,
+        rsiPeriod,
+        pivotLookbackRight,
+        pivotLookbackLeft,
+        minLookbackRange,
+        maxLookbackRange
+    );
+    console.log(result);
+}, 1000);
 
 module.exports = calculateRsi;
